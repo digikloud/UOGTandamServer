bca2e947fa925b5d3e4667f89d38b4a031f3efb4
Vigneshwaran V <vigneshwaranvengadesh@gmail.com>
2024-04-10 14:34:02 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import axios from 'axios';
+import querystring from 'querystring';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+app.use(bodyParser.json({ limit: '1mb' }));
+
+// set path for static content
+const staticPath = path.normalize(`${__dirname}/../app`);
+
+app.use(express.static(staticPath));
+// API endpoints
+app.post('/api/auth/token', async (req, res) => {
+  const token = await createToken();
+
+  res.status(200).json(token);
+});
+
+async function createToken() {
+  const options = {
+    'grant_type': 'client_credentials',
+    'scope': 'data:read viewables:read'
+  };
+  const response = await axios.post(`https://developer.api.autodesk.com/authentication/v2/token`,
+    querystring.stringify(options),
+    {
+      auth: {
+        username: process.env.APS_KEY,
+        password: process.env.APS_SECRET,
+      }
+    });
+  console.log("res",response.data)
+  return response.data;
+}
+
+// start listening
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.debug(`server is listening on port: ${port}`);
+});
